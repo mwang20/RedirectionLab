02458d271042c98c72ed297d49e110326a99f039
mwang20 <mwang20@stuy.edu>
2021-01-06 03:01:51 -0500

    Completed pigLatin method.

--- PigLatin.java
+++ PigLatin.java
@@ -24,7 +24,45 @@ public class PigLatin{
       translation += s.charAt(0);
       translation += "ay";
     }
+    
     return translation;
   }
 
+  public static String pigLatin(String s){
+    String[] digraphs = new String[]{"bl", "br", "ch", "ck", "cl", "cr", "dr", "fl", "fr", "gh", "gl", "gr", "ng", "ph", "pl", "pr", "qu", "sc", "sh", "sk", "sl", "sm", "sn", "sp", "st", "sw", "th", "tr", "tw", "wh", "wr"};
+    String translation = "";
+    boolean digraphStart = false;
+    
+    for (int i = 0; i < digraphs.length; i++){
+      if (s.substring(0, 2) == digraphs[i]){
+        digraphStart = true;
+      }
+    }
+    
+    if (s.charAt(0) == 'a'||s.charAt(0) =='e'||s.charAt(0) =='i'||s.charAt(0) =='o'||s.charAt(0) =='u'){
+      for (int i = 0; i < s.length(); i++){
+        translation += s.charAt(i);
+      }
+      translation += "hay";
+    }
+    
+    else if (digraphStart){
+      for (int i = 2; i < s.length(); i++){
+        translation += s.charAt(i);
+      }
+      translation += s.substring(0, 2);
+      translation += "ay";
+    }
+    
+    else {
+      for (int i = 1; i < s.length(); i++){
+        translation += s.charAt(i);
+      }
+      translation += s.charAt(0);
+      translation += "ay";
+    }
+    
+    return translation;
+
+  }
 }

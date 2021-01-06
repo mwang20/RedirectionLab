2b90b20891ddeb0ca7b6f0e8b07044c3f39dd77d
mwang20 <mwang20@stuy.edu>
2021-01-06 09:46:59 -0500

    I completed my main method for PigLatin class and tested it with sonnet.txt and expectedOut.txt using the terminal commands and the outputs were the same.

--- PigLatin.java
+++ PigLatin.java
@@ -3,7 +3,24 @@ import java.util.*;
 public class PigLatin{
 
   public static void main(String[] args){
+    
+    Scanner lines = new Scanner(System.in);
 
+    while (lines.hasNextLine()){
+
+      String currentLine = lines.nextLine();
+      Scanner words = new Scanner(currentLine);
+
+      System.out.println();
+
+      while (words.hasNext()){
+
+        String currentWord = words.next();
+        String translation = pigLatinBest(currentWord);
+
+        System.out.print(translation + " ");
+      }
+    }
   }
 
   public static String pigLatinSimple(String s){
@@ -66,7 +83,7 @@ public class PigLatin{
     return translation;
   }
   
-  public static String pigLatinBeast(String s){
+  public static String pigLatinBest(String s){
     
     String[] digraphs = new String[]{"bl", "br", "ch", "ck", "cl", "cr", "dr", "fl", "fr", "gh", "gl", "gr", "ng", "ph", "pl", "pr", "qu", "sc", "sh", "sk", "sl", "sm", "sn", "sp", "st", "sw", "th", "tr", "tw", "wh", "wr"};
     String translation = "";

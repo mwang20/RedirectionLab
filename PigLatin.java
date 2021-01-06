9d70d55c8998526c5d11199024959b1d4f591c8b
mwang20 <mwang20@stuy.edu>
2021-01-06 02:40:47 -0500

    Completed the pigLatinSimple method.

--- PigLatin.java
+++ PigLatin.java
@@ -6,4 +6,25 @@ public class PigLatin{
 
   }
 
+  public static String pigLatinSimple(String s){
+    
+    String translation = "";
+    
+    if (s.charAt(0) == 'a'||s.charAt(0) =='e'||s.charAt(0) =='i'||s.charAt(0) =='o'||s.charAt(0) =='u'){
+      for (int i = 0; i < s.length(); i++){
+        translation += s.charAt(i);
+      }
+      translation += "hay";
+    }
+    
+    else {
+      for (int i = 1; i < s.length(); i++){
+        translation += s.charAt(i);
+      }
+      translation += s.charAt(0);
+      translation += "ay";
+    }
+    return translation;
+  }
+
 }
